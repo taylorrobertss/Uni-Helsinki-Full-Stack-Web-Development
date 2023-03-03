b10cce7f93629404baf9bf2083e9e7b8682a7414
taylorrobertss <taylor_june@hotmail.com>
2023-03-03 09:32:09 +0200

    change port

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -74,6 +74,7 @@ let notes = [
     
   })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+  const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
