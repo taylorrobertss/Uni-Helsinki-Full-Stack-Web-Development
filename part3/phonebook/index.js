d1a0516939f18d37812388c34bcfdd5b107e155e
taylorrobertss <taylor_june@hotmail.com>
2023-03-15 17:50:04 +0200

    exercise 3.9 bug fix

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
-app.use(express.static('build'))
+
 const cors = require('cors')
 app.use(cors())
+// app.use(express.static('build'))
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
     console.log('Path:  ', request.path)
