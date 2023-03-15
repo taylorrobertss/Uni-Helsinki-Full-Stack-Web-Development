e97f1cd14cb25708d7300ab0221d1394387eaf85
taylorrobertss <taylor_june@hotmail.com>
2023-03-15 21:23:44 +0200

    exercises 3.13-3.14

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
-const morgan = require('morgan')
+app.use(express.static('build'))
 
+const morgan = require('morgan')
+require('dotenv').config()
 const cors = require('cors')
 app.use(cors())
-app.use(express.static('build'))
+
+const Person = require('./models/phonebook')
+
+
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
     console.log('Path:  ', request.path)
@@ -24,34 +29,14 @@ const requestLogger = (request, response, next) => {
   })
   app.use(morgan(':method :url :body') )
   
-let persons = [
-    { 
-        "id": 1,
-        "name": "Arto Hellas", 
-        "number": "040-123456"
-      },
-      { 
-        "id": 2,
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523"
-      },
-      { 
-        "id": 3,
-        "name": "Dan Abramov", 
-        "number": "12-43-234345"
-      },
-      { 
-        "id": 4,
-        "name": "Sam Poppendieck", 
-        "number": "39-23-6423122"
-      }
-  ]
   app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
   })
   
   app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+      response.json(persons)
+    })
   })
 
   app.get('/api/info', (request, response) => {
@@ -59,13 +44,9 @@ let persons = [
   })
 
   app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(persons => persons.id === id)
-    if (person) {
+    Person.findById(request.params.id).then(person => {
       response.json(person)
-    } else {
-      response.status(404).end()
-    }
+    })
   })
 
   app.delete('/api/persons/:id', (request, response) => {
@@ -82,32 +63,31 @@ let persons = [
   }
   app.post('/api/persons', (request, response) => {
     const body = request.body
-  
+    console.log("inside post")
     if (!body.name || !body.number ) {
       return response.status(400).json({ 
         error: 'content missing' 
       })
-    }
-    if(persons.map(n => n.name).includes(body.name)){
-        return response.status(400).json({ 
-            error: 'Name must be unique' 
-          })
-    }
+     }
+    // if(persons.map(n => n.name).includes(body.name)){
+    //     return response.status(400).json({ 
+    //         error: 'Name must be unique' 
+    //       })
+    // }
    
-    const person = {
+    const person = new Person({
       name: body.name,
       number: body.number,
-      id: generateId(),
-    }
+      // id: generateId(),
+    })
   
-    persons = persons.concat(person)
-  
-    response.json(person)
+    person.save().then(savedPerson => {
+      response.json(savedPerson)
+    })
     
   })
 
-
-  const PORT = process.env.PORT || 3001
+  const PORT = process.env.PORT
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
