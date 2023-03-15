69c57ae96660ce3f78d0a52b616e7a6fb2f7fcdc
taylorrobertss <taylor_june@hotmail.com>
2023-03-15 17:57:17 +0200

    exercise 3.09-3.11

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -4,7 +4,7 @@ const morgan = require('morgan')
 
 const cors = require('cors')
 app.use(cors())
-app.use(express.static('build'))
+//app.use(express.static('build'))
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
     console.log('Path:  ', request.path)
