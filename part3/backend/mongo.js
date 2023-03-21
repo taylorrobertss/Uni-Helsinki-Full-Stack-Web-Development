8219491004b69630cd8265e633ea78b8ffccc88a
taylorrobertss <taylor_june@hotmail.com>
2023-03-21 10:25:19 +0200

    notes app update with mongo db

---
+++ part3/backend/mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'Mongoose makes things easy',
+  date: new Date(),
+  important: true,
+})
+
+/*
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
+*/
+
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
